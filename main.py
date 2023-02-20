ad2f6c34cc5fd2baf2475bba210b3d02ead7fcbe
MrMeerman <>
2023-02-20 05:56:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Here I am defining the function named sumFunction
+def sumFunction(num1, num2):
+  sum = num1 + num2
+  return sum
+#---------------------------------------------------#
+# Here i am creating two variables and giving them values
+n1 = 4 # this is a variable
+n2 = 5 # this is also a variable 
+# Here I am calling the sumFunction function and putting it into an equation which is then printed to screen
+print(sumFunction(n1,n2) + 3)
+
+# Yipee doo jda dang dang ;ksjdnfjcn
